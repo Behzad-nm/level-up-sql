d5a97f34e5684ec38113dc0b9f9aacf500126a06
Behzad Nezhadmalayeri <behzad.nejadmalayeri@gmail.com>
2023-01-04 12:13:46 +0000

    4 or 5 more challenges

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,16 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT CustomerID, FirstName FROM Customers
+WHERE FirstName = 'Norby';
+
+SELECT * From Reservations WHERE CustomerID = 64;
+
+DELETE FROM Reservations WHERE ReservationID = 2000;
+
+-- better select with join:
+SELECT * FROM Reservations
+JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
+WHERE Customers.FirstName = 'Norby'
+AND Reservations.Date = '2022-07-29';
--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,11 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT Customers.LastName, Customers.CustomerID,
+  Reservations.ReservationID, Reservations.Date,
+  Reservations.PartySize
+FROM Reservations
+JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
+WHERE Customers.LastName LIKE 'Ste%'
+AND Reservations.PartySize = 4 AND Reservations.Date LIKE '%06-14%';
