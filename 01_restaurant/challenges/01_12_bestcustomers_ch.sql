eb365707cd72a82bdd3e78fe50652a85819b0bfa
Behzad Nezhadmalayeri <behzad.nejadmalayeri@gmail.com>
2023-01-05 08:36:35 +0000

    completeting the restaurant

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,11 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+-- firsname, lastname, email, sort by number of orders
+SELECT Customers.FirstName, Customers.LastName, Customers.Email,
+COUNT(Orders.OrderID) as OrderCount  
+FROM Customers JOIN Orders
+ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Orders.CustomerID
+ORDER BY OrderCount DESC
+LIMIT 10;
